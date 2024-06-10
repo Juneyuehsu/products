b60de89e5b5bb337b5dc0f25da8608e418032a26
Juneyuehsu <freebluebox@gmail.com>
2024-06-10 16:43:42 +0800

    add header

--- products.py
+++ products.py
@@ -17,3 +17,9 @@ print (products)
 
 for p in products:
 	print (p[0],'price is', p[1])
+
+# 把檔案存入
+with open ('products.csv', 'w', encoding='utf-8') as f:
+	f.write('product 品, price 價 \n')
+	for p in products:
+		f.write (p[0] + ',' + p[1] + '\n') # --> 4 個字串合在一個大字串, 丟給f.write
